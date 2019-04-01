1a2d91e4e4bb315706a4f762f13f28374872d9cb
Mariajo Leal <mariajo20leal@gmail.com>
2019-04-01 13:50:48 -0600

    Created routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,35 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
+})->name('blog.index');
+
+Route::get('article/{id}', function () {
+    return view('blog.article');
+})->name('blog.article');
+
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
+    
+    Route::get('create', function () {
+        return view('admin.create');
+    })->name('admin.create');
+    
+    Route::post('create', function () {
+        return "It works ";
+    })->name('admin.create');
+    
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+    
+    Route::post('edit', function () {
+        return "Updated";
+    })->name('admin.update');
 });
