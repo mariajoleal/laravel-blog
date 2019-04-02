aae5e746f8b62f7be02c3fca42fb6af5eeb53873
Mariajo Leal <mariajo20leal@gmail.com>
2019-04-02 10:27:19 -0600

    Added post model and controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Session\Store;
+
+class PostController extends Controller
+{
+    public function getIndex(Store $session) {
+        $post = new Post();
+        $posts = $post->getPosts($session);
+        return view('blog.index', ['posts' => $posts]);
+    }
+
+    public function getAdminIndex(Store $session) {
+        $post = new Post();
+        $posts = $post->getPosts($session);
+        return view('admin.index', ['posts' => $posts]);
+    }
+
+    public function getPost(Store $session, $id) {
+        $post = new Post();
+        $post = $post->getPost($session, $id);
+        return view('blog.article', ['post' => $post]);
+    } 
+
+    public function getAdminCreate() {
+        return view('admin.create');
+    }
+
+    public function getAdminEdit(Store $session, $id) {
+        $post = new Post();
+        $post = $post->getPost($session, $id);
+        return view('admin.edit', ['post' => $post, 'postId' => $id]);
+    }
+
+    public function postAdminCreate(Store $session, Request $request) {
+        $this->validate($request, [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+        $post = new Post();
+        $post = $post->addPost($session, $request->input('title'), $request->input('content'));
+        return redirect()->route('admin.index')->with('info', 'Article created, new Title ' . $request->input('title'));
+    }
+
+    public function postAdminUpdate(Store $session, Request $request) {
+        $this->validate($request, [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+        $post = new Post();
+        $post = $post->editPost($session, $request->input('id') , $request->input('title'), $request->input('content'));
+        return redirect()->route('admin.index')->with('info', 'Article updated, new Title ' . $request->input('title'));
+    }
+}
