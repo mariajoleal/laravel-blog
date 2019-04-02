234885a96323911f375746c35e1cb746f185b282
Mariajo Leal <mariajo20leal@gmail.com>
2019-04-02 10:40:07 -0600

    Modified routes

--- routes/web.php
+++ routes/web.php
@@ -11,76 +11,43 @@
 |
 */
 
-Route::get('/', function () {
-    return view('blog.index');
-})->name('blog.index');
+Route::get('/', [
+    'uses' => 'PostController@getIndex',
+    'as' => 'blog.index'
+]);
 
-Route::get('article/{id}', function ($id) {
-    if ($id == 1) {
-        $post = [
-            'title' => 'Learning laravel',
-            'content' => 'Some dummy content'
-        ];
-    } else {
-        $post = [
-            'title' => 'Learning laravel Article 2',
-            'content' => 'Some dummy content'
-        ];
-    }
-    return view('blog.article', ['post' => $post]);
-})->name('blog.article');
+Route::get('article/{id}', [
+    'uses' => 'PostController@getPost',
+    'as' => 'blog.article'
+]);
 
 Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('', function () {
-        return view('admin.index');
-    })->name('admin.index');
-    
-    Route::get('create', function () {
-        return view('admin.create');
-    })->name('admin.create');
-    
-    Route::post('create', function (\Illuminate\Http\Request $request,
-        \Illuminate\Validation\Factory $validator) {
-        $validation = $validator->make($request->all(), [
-            'title' => 'required',
-            'content' => 'required'
-        ]);
-        if ($validation->fails()) {
-            return redirect()->back()->withErrors($validation);
-        }
-        return redirect()->route('admin.index')
-        ->with('info', 'Article created, new Title ' . $request->input('title'));
-    })->name('admin.create');
-    
-    Route::get('edit/{id}', function ($id) {
-        if ($id == 1) {
-            $data = [
-                'title' => 'Learning laravel',
-                'content' => 'Some dummy content'
-            ];
-        } else {
-            $data = [
-                'title' => 'Learning laravel Article 2',
-                'content' => 'Some dummy content'
-            ];
-        }
-        return view('admin.edit', ['data' => $data]);
-    })->name('admin.edit');
-    
-    Route::post('edit', function (\Illuminate\Http\Request $request,
-    \Illuminate\Validation\Factory $validator) {
-    $validation = $validator->make($request->all(), [
-        'title' => 'required',
-        'content' => 'required'
+    Route::get('', [
+        'uses' => 'PostController@getAdminIndex',
+        'as' => 'admin.index'
     ]);
-    if ($validation->fails()) {
-        return redirect()->back()->withErrors($validation);
-    }
-        return redirect()->route('admin.index')
-        ->with('info', 'Article edited, new Title ' . $request->input('title'));
-    })->name('admin.update');
+    
+    Route::get('create', [
+        'uses' => 'PostController@getAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    
+    Route::post('create', [
+        'uses' => 'PostController@postAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    
+    Route::get('edit/{id}', [
+        'uses' => 'PostController@getAdminEdit',
+        'as' => 'admin.edit'
+    ]);
+    
+    Route::post('edit', [
+        'uses' => 'PostController@postAdminUpdate',
+        'as' => 'admin.update'
+    ]);
 });
