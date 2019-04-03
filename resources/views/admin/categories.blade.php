d97676a352e6cd76390a3ae7449c3a64cce876fa
Mariajo Leal <mariajo20leal@gmail.com>
2019-04-03 13:00:46 -0600

    Changed UI

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -4,7 +4,12 @@
   @if(Session::has('info'))
     <div class="row">
         <div class="col-md-12">
-          <p class="alert alert-info">{{ Session::get('info') }}</p>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ Session::get('info') }}                
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
         </div>
       </div>
   @endif
@@ -16,12 +21,25 @@
   <hr>
   <div class="row">
     <div class="col-md-12">
-      @foreach ($categories as $category)
-        <p><strong>{{$category->name}}</strong> 
-          <a href="{{ route('admin.editCategory', ['id' => $category->id ]) }}"> Edit </a>
-          <a href="{{ route('admin.deleteCategory', ['id' => $category->id ]) }}"> Delete </a>
-        </p>
-      @endforeach
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Article</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($categories as $category)
+            <tr>
+              <td>{{$category->name}}</td>
+              <td align="center">
+                <a style="color:#80bdff;" href="{{ route('admin.editCategory', ['id' => $category->id ]) }}">Edit</a> <br/>
+                <a style="color:#80bdff;" href="{{ route('admin.deleteCategory', ['id' => $category->id ]) }}">Delete</a> <br/>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
   </div>
 @endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,7 +4,12 @@
   @if(Session::has('info'))
     <div class="row">
         <div class="col-md-12">
-          <p class="alert alert-info">{{ Session::get('info') }}</p>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ Session::get('info') }}                
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
         </div>
       </div>
   @endif
@@ -16,12 +21,25 @@
   <hr>
   <div class="row">
     <div class="col-md-12">
-      @foreach ($posts as $post)
-        <p><strong>{{$post->title}}</strong> 
-          <a href="{{ route('admin.edit', ['id' => $post->id ]) }}"> Edit </a>
-          <a href="{{ route('admin.delete', ['id' => $post->id ]) }}"> Delete </a>
-        </p>
-      @endforeach
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Article</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($posts as $post)
+            <tr>
+              <td>{{$post->title}}</td>
+              <td align="center">
+                <a style="color:#80bdff;" href="{{ route('admin.edit', ['id' => $post->id ]) }}">Edit</a> <br/>
+                <a style="color:#80bdff;" href="{{ route('admin.delete', ['id' => $post->id ]) }}">Delete</a> <br/>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
   </div>
 @endsection
